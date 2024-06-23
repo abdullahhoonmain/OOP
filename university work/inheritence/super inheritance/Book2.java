2715097c7f995294b270932df35d735e859db5ea
Abdullah <116591300+abdullahhoonmain@users.noreply.github.com>
2024-06-23 21:34:50 +0500

    Add files via upload

---
+++ university work/inheritence/super inheritance/Book2.java
@@ -0,0 +1,14 @@
+package SuperInheritance;
+
+public class Book2 {
+    protected int pages;
+    public Book2 (int numpages){
+        pages= numpages;
+    }
+    public void setpages(int numpages){
+        pages= numpages;
+    }
+    public int getpages(){
+        return pages;
+    }
+}
---
+++ university work/inheritence/super inheritance/Dictionary2.java
@@ -0,0 +1,6 @@
+package SuperInheritance;
+
+public class Dictionary2 extends Book2 {
+    private  
+    
+}
